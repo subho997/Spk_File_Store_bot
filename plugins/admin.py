9ba55af6811cc18313224a74d097841cf2572fa8
suvo9848 <subhojitpatowari07@gmail.com>
2025-09-04 21:13:52 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -63,6 +63,97 @@ async def add_admins(client: Client, message: Message):
         )
 
 
+# Don't Remove Credit @CodeFlix_Bots, @rohit_1888
+# Ask Doubt on telegram @CodeflixSupport
+#
+# Copyright (C) 2025 by Codeflix-Bots@Github, < https://github.com/Codeflix-Bots >.
+#
+# This file is part of < https://github.com/Codeflix-Bots/FileStore > project,
+# and is released under the MIT License.
+# Please see < https://github.com/Codeflix-Bots/FileStore/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
+import asyncio
+import os
+import random
+import sys
+import time
+from pyrogram import Client, filters, __version__
+from pyrogram.enums import ParseMode, ChatAction, ChatMemberStatus, ChatType
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, ReplyKeyboardMarkup, ChatMemberUpdated, ChatPermissions
+from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, InviteHashEmpty, ChatAdminRequired, PeerIdInvalid, UserIsBlocked, InputUserDeactivated
+from bot import Bot
+from config import *
+from helper_func import *
+from database.database import *
+
+
+
+# Commands for adding admins by owner
+@Bot.on_message(filters.command('add_admin') & filters.private & filters.user(OWNER_ID))
+async def add_admins(client: Client, message: Message):
+    pro = await message.reply("<b><i>ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ..</i></b>", quote=True)
+    check = 0
+    admin_ids = await db.get_all_admins()
+    admins = message.text.split()[1:]
+
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="close")]])
+
+    if not admins:
+        return await pro.edit(
+            "<b>You need to provide user ID(s) to add as admin.</b>\n\n"
+            "<b>Usage:</b>\n"
+            "<code>/add_admin [user_id]</code> — Add one or more user IDs\n\n"
+            "<b>Example:</b>\n"
+            "<code>/add_admin 1234567890 9876543210</code>",
+            reply_markup=reply_markup
+        )
+
+    admin_list = ""
+    for id in admins:
+        try:
+            id = int(id)
+        except:
+            admin_list += f"<blockquote><b>Invalid ID: <code>{id}</code></b></blockquote>\n"
+            continue
+
+        if id in admin_ids:
+            admin_list += f"<blockquote><b>ID <code>{id}</code> already exists.</b></blockquote>\n"
+            continue
+
+        id = str(id)
+        if id.isdigit() and len(id) == 10:
+            admin_list += f"<b><blockquote>(ID: <code>{id}</code>) added.</blockquote></b>\n"
+            check += 1
+        else:
+            admin_list += f"<blockquote><b>Invalid ID: <code>{id}</code></b></blockquote>\n"
+
+    if check == len(admins):
+        for id in admins:
+            await db.add_admin(int(id))
+        await pro.edit(f"<b>✅ Admin(s) added successfully:</b>\n\n{admin_list}", reply_markup=reply_markup)
+    else:
+        await pro.edit(
+            f"<b>❌ Some errors occurred while adding admins:</b>\n\n{admin_list.strip()}\n\n"
+            "<b><i>Please check and try again.</i></b>",
+            reply_markup=reply_markup
+        )
+
+
+# Don't Remove Credit @CodeFlix_Bots, @rohit_1888
+# Ask Doubt on telegram @CodeflixSupport
+#
+# Copyright (C) 2025 by Codeflix-Bots@Github, < https://github.com/Codeflix-Bots >.
+#
+# This file is part of < https://github.com/Codeflix-Bots/FileStore > project,
+# and is released under the MIT License.
+# Please see < https://github.com/Codeflix-Bots/FileStore/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
 @Bot.on_message(filters.command('deladmin') & filters.private & filters.user(OWNER_ID))
 async def delete_admins(client: Client, message: Message):
     pro = await message.reply("<b><i>ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ..</i></b>", quote=True)
@@ -96,6 +187,44 @@ async def delete_admins(client: Client, message: Message):
                 id = int(admin_id)
             except:
                 passed += f"<blockquote><b>Invalid ID: <code>{admin_id}</code></b></blockquote>\n"
+                continue
+
+            if id in admin_ids:
+                await db.del_admin(id)
+                passed += f"<blockquote><code>{id}</code> ✅ Removed</blockquote>\n"
+            else:
+                passed += f"<blockquote><b>ID <code>{id}</code> not found in admin list.</b></blockquote>\n"
+
+        await pro.edit(f"<b>⛔️ Admin removal result:</b>\n\n{passed}", reply_markup=reply_markup)
+    else:
+        await pro.edit("<b><blockquote>No admin IDs available to delete.</blockquote></b>", reply_markup=reply_markup)
+
+
+@Bot.on_message(filters.command('admins') & filters.private & admin)
+async def get_admins(client: Client, message: Message):
+    pro = await message.reply("<b><i>ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ..</i></b>", quote=True)
+    admin_ids = await db.get_all_admins()
+
+    if not admin_ids:
+        admin_list = "<b><blockquote>❌ No admins found.</blockquote></b>"
+    else:
+        admin_list = "\n".join(f"<b><blockquote>ID: <code>{id}</code></blockquote></b>" for id in admin_ids)
+
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="close")]])
+    await pro.edit(f"<b>⚡ Current Admin List:</b>\n\n{admin_list}", reply_markup=reply_markup)
+
+# Don't Remove Credit @CodeFlix_Bots, @rohit_1888
+# Ask Doubt on telegram @CodeflixSupport
+#
+# Copyright (C) 2025 by Codeflix-Bots@Github, < https://github.com/Codeflix-Bots >.
+#
+# This file is part of < https://github.com/Codeflix-Bots/FileStore > project,
+# and is released under the MIT License.
+# Please see < https://github.com/Codeflix-Bots/FileStore/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+ <code>{admin_id}</code></b></blockquote>\n"
                 continue
 
             if id in admin_ids:
