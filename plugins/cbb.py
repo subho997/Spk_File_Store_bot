ca7a9ea75b7dafaa1a52393e662f9e8e54132e96
suvo9848 <subhojitpatowari07@gmail.com>
2025-09-05 21:55:48 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -22,7 +22,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             text=HELP_TXT.format(first=query.from_user.first_name),
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup([
-                [InlineKeyboardButton('ʜᴏᴍᴇ', callback_data='start'),
+                [InlineKeyboardButton("ʜᴏᴍᴇ", callback_data='start'),
                  InlineKeyboardButton("ᴄʟᴏꜱᴇ", callback_data='close')]
             ])
         )
@@ -69,11 +69,11 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             caption=(
                 f"👋 {query.from_user.username}\n\n"
                 f"🎖️ Available Plans :\n\n"
-                f"● {PRICE1}  For 0 Days Prime Membership\n\n"
-                f"● {PRICE2}  For 1 Month Prime Membership\n\n"
-                f"● {PRICE3}  For 3 Months Prime Membership\n\n"
-                f"● {PRICE4}  For 6 Months Prime Membership\n\n"
-                f"● {PRICE5}  For 1 Year Prime Membership\n\n\n"
+                f"● {PRICE1}  For 3 Days Prime Membership\n\n"
+                f"● {PRICE2}  For 7 Days Prime Membership\n\n"
+                f"● {PRICE3}  For 1 Months Prime Membership\n\n"
+                f"● {PRICE4}  For 2 Months Prime Membership\n\n"
+                f"● {PRICE5}  For 3 Year Prime Membership\n\n\n"
                 f"💵 ASK UPI ID TO ADMIN AND PAY THERE -  <code>{UPI_ID}</code>\n\n\n"
                 f"♻️ After Payment You Will Get Instant Membership \n\n\n"
                 f"‼️ Must Send Screenshot after payment & If anyone want custom time membrship then ask admin"
