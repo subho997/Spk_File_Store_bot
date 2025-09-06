d509d85c50fb4e533136a8e12955b35b4b3b429d
suvo9848 <subhojitpatowari07@gmail.com>
2025-09-06 22:28:38 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -35,7 +35,7 @@ START_TIME = time.time()
 def get_readable_time(seconds: int) -> str:
     count = 0
     time_list = []
-    time_suffix_list = ["s", "m", "h", "days"]
+    time_suffix_list = ["days", "h", "m", "s"]
 
     while count < 4:
         count += 1
