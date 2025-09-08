42f7838a26db2e43995a6d64ed583aa473c097c9
subho997 <subhojitpatowari07@gmail.com>
2025-09-08 17:38:29 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -1,39 +1,21 @@
 # Don't Remove Credit @CodeFlix_Bots, @rohit_1888
-
 # Ask Doubt on telegram @CodeflixSupport
-
 #
-
 # Copyright (C) 2025 by Codeflix-Bots@Github, < https://github.com/Codeflix-Bots >.
-
 #
-
 # This file is part of < https://github.com/Codeflix-Bots/FileStore > project,
-
 # and is released under the MIT License.
-
 # Please see < https://github.com/Codeflix-Bots/FileStore/blob/master/LICENSE >
-
 #
-
 # All rights reserved.
-
 #
-
-
 
 import asyncio
-
 import os
-
 import random
-
 import sys
-
 import time
-
 from datetime import datetime, timedelta
-
 from pyrogram import Client, filters, __version__
 from pyrogram.enums import ParseMode, ChatAction
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, ReplyKeyboardMarkup, ChatInviteLink, ChatPrivileges
@@ -46,201 +28,94 @@ from database.database import *
 
 #=====================================================================================##
 
-
-
 # Capture the bot start time
-
 START_TIME = time.time()
 
-
-
 # Helper function to format uptime nicely
-
 def get_readable_time(seconds: int) -> str:
-
     days, seconds = divmod(seconds, 86400)
-
     hours, seconds = divmod(seconds, 3600)
-
     minutes, seconds = divmod(seconds, 60)
 
-
-
     time_parts = []
-
     if days > 0:
-
         time_parts.append(f"{days}d")
-
     if hours > 0:
-
         time_parts.append(f"{hours}h")
-
     if minutes > 0:
-
         time_parts.append(f"{minutes}m")
-
     if seconds > 0:
-
         time_parts.append(f"{seconds}s")
-
-
 
     return " ".join(time_parts)
 
 
-
-
-
 # Stats command
-
 @Bot.on_message(filters.command("stats"))
-
 async def stats(bot: Bot, message: Message):
-
     current_time = time.time()
-
     uptime = get_readable_time(int(current_time - START_TIME))
-
     await message.reply(
-
         f"🤖 BOT UPTIME\n{uptime}"
-
     )
 
-
-
 #=====================================================================================##
-
-
 
 WAIT_MSG = "<b>Working....</b>"
 
-
-
 #=====================================================================================##
-
-
-
 
 
 @Bot.on_message(filters.command('users') & filters.private & admin)
-
 async def get_users(client: Bot, message: Message):
-
     msg = await client.send_message(chat_id=message.chat.id, text=WAIT_MSG)
-
     users = await db.full_userbase()
-
     await msg.edit(f"{len(users)} users are using this bot")
 
-
-
 # Don't Remove Credit @CodeFlix_Bots, @rohit_1888
-
 # Ask Doubt on telegram @CodeflixSupport
-
 #
-
 # Copyright (C) 2025 by Codeflix-Bots@Github, < https://github.com/Codeflix-Bots >.
-
 #
-
 # This file is part of < https://github.com/Codeflix-Bots/FileStore > project,
-
 # and is released under the MIT License.
-
 # Please see < https://github.com/Codeflix-Bots/FileStore/blob/master/LICENSE >
-
 #
-
 # All rights reserved.
-
 #
-
-
 
 #=====================================================================================##
-
-
 
 #AUTO-DELETE
 
-
-
 @Bot.on_message(filters.private & filters.command('dlt_time') & admin)
-
 async def set_delete_time(client: Bot, message: Message):
-
     try:
-
         duration = int(message.command[1])
-
-
 
         await db.set_del_timer(duration)
 
-
-
         await message.reply(f"<b>Dᴇʟᴇᴛᴇ Tɪᴍᴇʀ ʜᴀs ʙᴇᴇɴ sᴇᴛ ᴛᴏ <blockquote>{duration} sᴇᴄᴏɴᴅs.</blockquote></b>")
 
-
-
     except (IndexError, ValueError):
-
         await message.reply("<b>Pʟᴇᴀsᴇ ᴘʀᴏᴠɪᴅᴇ ᴀ ᴠᴀʟɪᴅ ᴅᴜʀᴀᴛɪᴏɴ ɪɴ sᴇᴄᴏɴᴅs.</b> Usage: /dlt_time {duration}")
 
-
-
 @Bot.on_message(filters.private & filters.command('check_dlt_time') & admin)
-
 async def check_delete_time(client: Bot, message: Message):
-
     duration = await db.get_del_timer()
-
-
 
     await message.reply(f"<b><blockquote>Cᴜʀʀᴇɴᴛ ᴅᴇʟᴇᴛᴇ ᴛɪᴍᴇʀ ɪs sᴇᴛ ᴛᴏ {duration}sᴇᴄᴏɴᴅs.</blockquote></b>")
 
-
-
 #=====================================================================================##
 
-
-
-
-@Bot.on_message(filters.private & filters.incoming)
-
-
-async def useless(_,message: Message):
-
-
-    if USER_REPLY_TEXT:
-
-
-        await message.reply(USER_REPLY_TEXT)
-
-
-
-
 # Don't Remove Credit @CodeFlix_Bots, @rohit_1888
-
 # Ask Doubt on telegram @CodeflixSupport
-
 #
-
 # Copyright (C) 2025 by Codeflix-Bots@Github, < https://github.com/Codeflix-Bots >.
-
 #
-
 # This file is part of < https://github.com/Codeflix-Bots/FileStore > project,
-
 # and is released under the MIT License.
-
 # Please see < https://github.com/Codeflix-Bots/FileStore/blob/master/LICENSE >
-
 #
-
 # All rights reserved.
-
 #
