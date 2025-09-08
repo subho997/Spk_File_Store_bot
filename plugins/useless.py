ecaf2fa9ad1bedc75925f0981111947b8d05c4ab
subho997 <subhojitpatowari07@gmail.com>
2025-09-08 15:30:46 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -22,7 +22,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated, UserNotParticipant
 from bot import Bot
-from config import ADMINS, BOT_STATS_TEXT, USER_REPLY_TEXT
+from config import *
 from helper_func import *
 from database.database import *
 
