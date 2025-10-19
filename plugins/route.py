d184a84a9916c0369cd3eddb0a0fad8beb030fcf
subho997 <subhojitpatowari33@gmail.com>
2025-10-19 11:49:51 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -4,4 +4,8 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("Codeflix FileStore")
+    return web.json_response({
+        "server_status": "running",
+        "bot": "@Spk_File_Store_Bot",
+        "made_by": "@spk_links"
+    })
