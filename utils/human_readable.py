6f9ec99e5ad514f59327a5b925be6a37cfb2da98
subho997 <subhojitpatowari33@gmail.com>
2025-10-19 13:33:46 +0530

    Create human_readable.py

---
+++ utils/human_readable.py
@@ -0,0 +1,10 @@
+def humanbytes(size):
+    if not size:
+        return ""
+    power = 2**10
+    n = 0
+    Dic_powerN = {0: ' ', 1: 'K', 2: 'M', 3: 'G', 4: 'T'}
+    while size > power:
+        size /= power
+        n += 1
+    return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
